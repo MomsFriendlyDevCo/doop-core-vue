507c37262eb0328774451d87c118e148ead1ac59
Matt Carter <m@ttcarter.com>
2021-04-04 15:44:07 +1000

    fix: Kludge to patch up vue-loader always assuming a module exports something

--- fixes/vue-noexport.js
+++ fixes/vue-noexport.js
@@ -1,3 +1,6 @@
+/**
+* Patch inline loaded .vue files so that app.component() is always preceeded by `export default`
+*/
 module.exports = function VueNoExportFix (source) {
 	return source.replace(/^(\s*)(app\.component\()/m, '$1export default $2');
 };
