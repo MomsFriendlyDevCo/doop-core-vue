8e4defce5f42f7df8b02a3dbe49d58c10e439b49
Mr-Yellow <mr-yellow@mr-yellow.com>
2021-04-06 09:26:26 +1000

    fix: Extended custom loader and removed perl krudge

--- fixes/vue-noexport.js
+++
@@ -1,6 +0,0 @@
-/**
-* Patch inline loaded .vue files so that app.component() is always preceeded by `export default`
-*/
-module.exports = function VueNoExportFix (source) {
-	return source.replace(/^(\s*)(app\.component\()/m, '$1export default $2');
-};
