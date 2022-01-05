07c93720b104c9a031bbdf86da23d70dc78ab970
Mr-Yellow <mr-yellow@mr-yellow.com>
2022-01-06 07:51:46 +1000

    feat: Importing of *.doop directly

--- doop.backend.hooks.js
+++
@@ -1,6 +0,0 @@
-/**
-* Hook file for loading into the Doop core
-*/
-if (!global.app) throw new Error('Cant find `app` global - run this hook file within a Doop project only');
-
-require('./lib/expressMiddleware');
